cd16d78743aaac18e7494d14552ad7a0fa0274d7
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-17 15:07:06 -0400

    Added paths /set and /fetch to test scope between responses

--- express_server.js
+++ express_server.js
@@ -26,6 +26,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
+// //Setup /set and /fetch path tests
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
+
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
+
 //Setup listener for requests
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
