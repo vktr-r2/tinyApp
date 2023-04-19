c8633158d1db919f081565fecfb6bcbcd354a6d7
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-19 15:34:42 -0400

    Add login functionalit, capture username in cookie, display username in header on all pages in app

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 // Import express / Assign it to variable 'app' for convenience / Set default port
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 
@@ -10,6 +11,7 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 //Decode input from front end to be able to work with in back end using .urlencoded
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // List of URLs
 const urlDatabase = {
@@ -49,13 +51,21 @@ app.get("/hello", (req, res) => {
 
 //Response for /urls path >> set templateVars to urls obj, call res.render to render urls_index using templateVars
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const currentUsername = req.cookies["username"];
+  const templateVars = {
+    urls: urlDatabase,
+    user: currentUsername
+  };
   res.render("urls_index", templateVars);
 });
 
 //Response for /urls/new path
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const currentUsername = req.cookies["username"];
+  const templateVars = {
+    user: currentUsername
+  };
+  res.render("urls_new", templateVars);
 });
 
 
@@ -69,7 +79,7 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   let id = generateRandomString();  //Assign my random string output to id
   urlDatabase[id] = req.body.longURL;  //Add id and longURL propert to existing urlDatabase object
-  res.redirect(`/urls/${id}`);  //Use res.redirect to redirect user to the new id in browser 
+  res.redirect(`/urls/${id}`);  //Use res.redirect to redirect user to the new id in browser
 });
 
 //Handles POST route that DELETEs URL from our urlDatabase object (triggered by delete button)
@@ -81,13 +91,13 @@ app.post("/urls/:id/delete", (req, res) => {
 //Handles POST route that redirects user to the Edit page (triggered by Edit button)
 app.post("/urls/:id/edit", (req, res) => {
   res.redirect(`/urls/${req.params.id}`);  //Redirect to the urlDatabase[id] page
-});  
+});
 
 //Handles POST route that submits the updated URL to our urlDatabase object (triggered by Submit button)
 app.post("/urls/:id/submit", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls/");  //Use res.redirect to redirect user to the urls index page after submit
-});                        
+});
 
 
 
@@ -96,10 +106,10 @@ app.post("/urls/:id/submit", (req, res) => {
 // vvv LOGIN & COOKIES vvv //
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie('username', username, { path: '/' });
-  res.redirect('/urls');
-}); 
+  const user = req.body.username;
+  res.cookie('username', user);
+  res.redirect(`/urls/`); //
+});
 
 
 
@@ -117,13 +127,14 @@ app.get("/u/:id", (req, res) => {
 
 //Handles /urls/:id path.  id and longURL passed to ejs template through templateVars variable
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-    res.render("urls_show", templateVars);
-  });
+  const currentUsername = req.cookies["username"];
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: currentUsername };
+  res.render("urls_show", templateVars);
+});
   
     
     
-    //Setup listener for requests
-    app.listen(PORT, () => {
-      console.log(`Vik's TinyURL app listening on port ${PORT}!`);
-    });
+//Setup listener for requests
+app.listen(PORT, () => {
+  console.log(`Vik's TinyURL app listening on port ${PORT}!`);
+});
