6cc59416617e3b6b9ca6d8fc357a485af816abdf
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-20 20:02:53 -0400

    Organized code, resolved header errors that were returning

--- express_server.js
+++ express_server.js
@@ -5,6 +5,10 @@ const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080;
 
+//Setup listener for requests
+app.listen(PORT, () => {
+  console.log(`Vik's TinyURL app listening on port ${PORT}!`);
+});
 
 //<<<<< MIDDLEWARE >>>>>\\
 app.set("view engine", "ejs");
@@ -75,7 +79,7 @@ const userLookup = (email) => {
 
 //Lookup URLs for user
 const urlsForUser = (id) => {
-  const myURLs = {}
+  const myURLs = {};
   for (const URL in urlDatabase) {
     // console.log(URL);
     // console.log(urlDatabase[URL].userID);
@@ -92,6 +96,7 @@ const urlsForUser = (id) => {
 //GET homepage(login page)
 app.get("/", (req, res) => {
   res.render("urls_login");
+  return;
 });
 
 
@@ -99,39 +104,45 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const currentUser = users[req.cookies["user"]];
   if (currentUser) {
-  myURLs = urlsForUser(currentUser.id);
-  const templateVars = {
-    urls: myURLs,
-    currentUser
-  };
-  res.render("urls_index", templateVars);
-}
-res.render("urls_login")
+    const myURLs = urlsForUser(currentUser.id);
+    const templateVars = {
+      urls: myURLs,
+      currentUser
+    };
+    res.render("urls_index", templateVars);
+    return;
+  }
+  res.render("urls_login");
+  return;
 });
 
 
 //GET NEW URL page
 app.get("/urls/new", (req, res) => {
   if (!req.cookies["user"]) {
-    res.render("urls_login")
+    res.render("urls_login");
     return;
   }
   const currentUser = users[req.cookies["user"]];
   const templateVars = { currentUser };
   res.render("urls_new", templateVars);
+  return;
 });
 
 //GET registration form page
 app.get("/register", (req, res) => {
   res.render("urls_register");
+  return;
 });
 
 //GET login form page
 app.get("/login", (req, res) => {
   if (!req.cookies["user"]) {
-    res.render("urls_login")
+    res.render("urls_login");
+    return;
   }
-  res.redirect("/urls")
+  res.redirect("/urls");
+  return;
 });
 
 //GET longURL page when short URL used
@@ -139,10 +150,12 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id].longURL;
 
   if (longURL === undefined) {
-    res.status(404).send('URL not found')
+    res.status(404).send('URL not found');
+    return;
   }
 
   res.redirect(longURL);
+  return;
 });
 
 
@@ -150,53 +163,62 @@ app.get("/u/:id", (req, res) => {
 
 //<<<<< BUTTONS >>>>>\\
 
-//Handles post responses coming in from submission form (/urls/new path)
+//POST new URL object for user
 app.post("/urls", (req, res) => {
+  //If not logged in, throw 403
   if (!req.cookies["user"]) {
-    res.status(403).send('Must be logged in to submit URLs')
+    res.status(403).send('Must be logged in to submit URLs');
     return;
-    }
-  const id = generateRandomString();  //Assign my random string output to id
-  let date = new Date();              //Find date for new URL object 
+  }
+  //Store new created_at date and unique ID for URL object
+  const id = generateRandomString();
+  let date = new Date();
   date = date.toLocaleDateString();
 
-  //create new URL object, store url id,
+  //Create new URL object
   urlDatabase[id] = {
     id,
     longURL: req.body.longURL,
     userID: req.cookies["user"],
-    createdAt: date
+    createdAt: date,
+    usageTally: 0
   };
-  res.redirect(`/urls/${id}`);  //Use res.redirect to redirect user to the new id in browser
+  //Redirect user to new URL's page
+  res.redirect(`/urls/${id}`);
   return;
 });
 
-//Handles POST route that DELETEs URL from our urlDatabase object (triggered by delete button)
+//POST DELETE existing URL object (delete button)
 app.post("/urls/:id/delete", (req, res) => {
-
+  //Set currentUser to cookie "user"
   const currentUser = users[req.cookies["user"]];
-  myURLs = urlsForUser(currentUser.id);
+  //Set myURLs to match URLs avail for currentUser
+  const myURLs = urlsForUser(currentUser.id);
+  //If URL to be changed is in myURLs for user, delete
   for (const URL in myURLs) {
     if (req.params.id === URL) {
       delete urlDatabase[req.params.id];
       res.redirect("/urls/");
-    }}
-
-    res.status(403).send('Forbidden : You do not have access to this page.')
-
+      return;
+    }
+  }
+  //If user doesn't have permission to delete, throw 403
+  res.status(403).send('Forbidden : You do not have access to this page.');
+  return;
 });
 
 
-//Handles POST route that redirects user to the Edit page (triggered by Edit button)
+//POST redirects user to the URL/Edit page
 app.post("/urls/:id/edit", (req, res) => {
   res.redirect(`/urls/${req.params.id}`);  //Redirect to the urlDatabase[id] page
+  return;
 });
 
-//Handles POST route that submits the updated URL to our urlDatabase object (triggered by Submit button)
+//POST submit new long URL to existing shortened URL id
 app.post("/urls/:id/submit", (req, res) => {
   urlDatabase[req.params.id].longURL = req.body.longURL;
-  // console.log(urlDatabase);
-  res.redirect("/urls/");  //Use res.redirect to redirect user to the urls index page after submit
+  res.redirect("/urls/");
+  return;
 });
 
 
@@ -208,14 +230,16 @@ app.post("/urls/:id/submit", (req, res) => {
 
 //POST Handle login, set username to cookie
 app.post("/login", (req, res) => {
-
+  //Store email and password from form
   const email = req.body.email;
   const password = req.body.password;
+  //Store result of userToVerify function for email that was submitted
   const userToVerify = userLookup(email);
 
   //Email doesn't exist
   if (userToVerify === null) {
     res.status(403).send('Email or password does not match');
+    return;
   }
 
   //Email exists
@@ -223,23 +247,26 @@ app.post("/login", (req, res) => {
     //Wrong password
     if (bcrypt.compareSync(password, userToVerify.password) === false) {
       res.status(403).send('Email or password does not match');
+      return;
     }
     //Correct password
     if (bcrypt.compareSync(password, userToVerify.password) === true) {
       const userId = userToVerify.id;
       res.cookie("user", userId);
+      return;
     }
   }
 
   res.redirect("/urls/");
+  return;
 });
 
 //POST Logout, clear cookie
 app.post("/logout", (req, res) => {
-  res.clearCookie("user");
-  res.redirect("/login"); //
+  res.clearCookie("user")
+  res.redirect("/login");
+  return;
 });
-
 
 //POST register new user
 app.post("/register", (req, res) => {
@@ -247,6 +274,7 @@ app.post("/register", (req, res) => {
   //Check if user exists
   if (userLookup(req.body.email) !== null) {
     res.status(400).send('User already registered. Please try to login.');
+    return;
   }
 
   //Check email and password both entered
@@ -265,8 +293,9 @@ app.post("/register", (req, res) => {
       password: bcrypt.hashSync(req.body.password, 10)};
     
     //Set cookie to logged-in state
-    res.cookie("user" , userId);
+    res.cookie("user" , userId)
     res.redirect("/urls/");
+    return;
   }
 });
 
@@ -276,23 +305,23 @@ app.get("/urls/:id", (req, res) => {
   const currentUser = users[req.cookies["user"]];
 
   if (!currentUser) {
-    res.status(403).send('Forbidden : You do not have access to this page.')
+    res.status(403).send('Forbidden : You do not have access to this page.');
+    return;
   }
 
-  myURLs = urlsForUser(currentUser.id);
+  const myURLs = urlsForUser(currentUser.id);
   
   for (const URL in myURLs) {
     if (req.params.id === URL) {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, currentUser };
-  res.render("urls_show", templateVars);
-    }}
+      const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, currentUser };
+      res.render("urls_show", templateVars);
+      return;
+    }
+  }
 
-  res.status(403).send('Forbidden : You do not have access to this page.')
+  res.status(403).send('Forbidden : You do not have access to this page.');
+  return;
 });
   
     
     
-//Setup listener for requests
-app.listen(PORT, () => {
-  console.log(`Vik's TinyURL app listening on port ${PORT}!`);
-});
